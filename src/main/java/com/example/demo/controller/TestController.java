a005190b64d7633120e38a1f26efdf512bb7a92f
JANG-90 <wahall90@gmail.com>
2023-11-02 00:41:39 +0900

    11.2

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
-import org.hibernate.annotations.GeneratorType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test") //리소스
@@ -15,8 +17,35 @@ public class TestController {
     }
 
     @GetMapping("/{id}")
-    public String testControllerWithPathVariables(@PathVariable(required = false)int id) {
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
         return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! Id " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestbody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! ID " + testRequestBodyDTO.getId()  + " Massage : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody(){
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! i'm ReponseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testContollerResponseEntity(){
+        List<String> list = new ArrayList<>();
+        list.add("Hello World I'm ResponseEntity. And you got 400!");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        //http status 를 400로 설정.
+        return ResponseEntity.ok().body(response);
     }
 
 }
